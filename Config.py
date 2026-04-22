a535b703a2879120d2fa4bbd51509f00e440e3e4
Sumathi R <33181655+sujiram@users.noreply.github.com>
2026-04-22 05:20:23 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,6 +6,5 @@ POSTGRES_DB = os.getenv("POSTGRES_DB", "analytics")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "etl_user")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "etl_password")
 
-#RAW_FILE_PATH = os.getenv("RAW_FILE_PATH", "/opt/airflow/data/raw_logs.json")
 
 RAW_FILE_PATH = os.getenv("RAW_FILE_PATH", "raw_logs.json")
